0565c9530b5f326fb2d1e0ce281e7697f46463e7
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-12-11 00:37:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,29 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 
-# Load the resaved model using tf.keras.models.load_model
+# Load the resaved model using tf.saved_model.load
 model_path = "resaved_sales_forecast_model"
-model = tf.keras.models.load_model(model_path)
+model = tf.saved_model.load(model_path)
+
+# Get the concrete function
+concrete_func = model.signatures[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
 
 def predict_sales(input_sequence):
-    input_sequence = np.array(input_sequence).reshape(1, len(input_sequence), len(input_sequence[0]))
-    predictions = model.predict(input_sequence)
-    return predictions.flatten().tolist()
+    # Convert the input string to a list of lists
+    input_sequence = [list(map(float, row.split(','))) for row in input_sequence.split('\n')]
+
+    # Preprocess the input
+    input_array = np.array(input_sequence).reshape(1, len(input_sequence), len(input_sequence[0]))
+
+    # Make predictions
+    predictions = concrete_func(tf.constant(input_array))
+    return predictions['dense'].numpy().flatten().tolist()
 
 def main():
     st.title("Sales Forecasting App")
 
     # Get input sequence from the user
     input_sequence = st.text_area("Enter your input sequence (comma-separated values):")
-
-    # Convert the input string to a list of lists
-    input_sequence = [list(map(float, row.split(','))) for row in input_sequence.split('\n')]
 
     if st.button("Predict"):
         # Make predictions
@@ -32,4 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
