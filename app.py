f61d741603b8b062f79b0411b779bb577a46d97f
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-12-10 21:29:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+import numpy as np
+
+# Load the trained model
+model = load_model("sales_forecast_model.h5")
+
+def predict_sales(input_sequence):
+    input_sequence = np.array(input_sequence).reshape(1, len(input_sequence), len(input_sequence[0]))
+    predictions = model.predict(input_sequence)
+    return predictions.flatten().tolist()
+
+def main():
+    st.title("Sales Forecasting App")
+
+    # Get input sequence from the user
+    input_sequence = st.text_area("Enter your input sequence (comma-separated values):")
+
+    # Convert the input string to a list of lists
+    input_sequence = [list(map(float, row.split(','))) for row in input_sequence.split('\n')]
+
+    if st.button("Predict"):
+        # Make predictions
+        predictions = predict_sales(input_sequence)
+
+        # Display the predictions
+        st.write("Predicted Sales:")
+        st.write(predictions)
+
+if __name__ == "__main__":
+    main()
