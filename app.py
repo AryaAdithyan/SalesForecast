e6a11c95c96109da0b175566c342176a1b203926
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-12-10 23:48:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 
-# Load the trained model
-# Load your model using TensorFlow SavedModel format
-model = tf.keras.models.load_model("sales_forecast_model_saved_model")
+# Load the trained model using h5py
+model_path = "sales_forecast_model_saved_model"
+model = h5py.File(model_path, 'r')
 
 def predict_sales(input_sequence):
     input_sequence = np.array(input_sequence).reshape(1, len(input_sequence), len(input_sequence[0]))
-    predictions = model.predict(input_sequence)
+    predictions = model['model'].predict(input_sequence)
     return predictions.flatten().tolist()
 
 def main():
@@ -31,4 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
