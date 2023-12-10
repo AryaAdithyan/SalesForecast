687b949d3eb2677d8604758a17ddec1e672af693
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-12-11 00:19:05 +0530

    Update resave_model.py

--- resave_model.py
+++ resave_model.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Load the original model
-original_model = tf.keras.models.load_model("sales_forecast_model_saved_model")
+original_model = tf.keras.models.load_model("sales_forecast_model_saved_model.py")
 
 # Save the model in a different format
 tf.keras.models.save_model(original_model, "resaved_sales_forecast_model")
