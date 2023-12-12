410cfb3cd37cd3d39b4ba6a994abd5a3ae8eab25
AryaAdithyan <120321472+AryaAdithyan@users.noreply.github.com>
2023-12-12 14:34:05 +0530

    Update resave_model.py

--- resave_model.py
+++ resave_model.py
@@ -1,10 +1,8 @@
 # resave_model.py
 
 import os
+import shutil
 import tensorflow as tf
-
-# Print the current working directory
-print("Current Working Directory:", os.getcwd())
 
 # Load the original model
 original_model = tf.keras.models.load_model("sales_forecast_model_saved_model")
@@ -15,8 +13,8 @@ export_dir = "resaved_sales_forecast_model"
 # Ensure the directory exists
 os.makedirs(export_dir, exist_ok=True)
 
-# Print the contents of the current directory
-print("Contents of Current Directory:", os.listdir())
-
 # Save the model in the SavedModel format
 tf.saved_model.save(original_model, export_dir)
+
+# Optionally, copy the variables folder to the new directory
+shutil.copytree("sales_forecast_model_saved_model/variables", f"{export_dir}/variables")
